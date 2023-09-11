0d12b8c63adb491d0cc948bf71f652ca61fa74c5
CarlosEmartinezC <133069933+CarlosEmartinezC@users.noreply.github.com>
2023-09-10 21:06:34 -0500

    listo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,15 +10,15 @@ const Contact = () => {
     <div className='container mx-auto'>
       <div className='flex flex-col lg:flex-row'>
         {/*text*/}
-        <div className='flex-1 '>
+        <motion.div variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3 }} className='flex-1 '>
           <div>
             <h4 className='text-q uppercase text-accent font-medium mb-2 tracking-wide'>Get in touch</h4>
             <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Let's work <br/> together!
             </h2>
           </div>
-        </div>
+        </motion.div>
         {/*from*/}
-        <from className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
+        <motion.from variants={fadeIn('left', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3 }} className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
           <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
           type='text'
           placeholder='Your name' />
@@ -28,7 +28,7 @@ const Contact = () => {
           <textarea className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none'
           placeholder='Your message'></textarea>
           <button className='btn btn-lg'>Send message</button>
-        </from>
+        </motion.from>
       </div>
     </div>
   </section>
