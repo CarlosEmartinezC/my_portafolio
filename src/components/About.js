ad0e6df56b6afb52a5803060c20277e83c91a42a
CarlosEmartinezC <133069933+CarlosEmartinezC@users.noreply.github.com>
2023-09-12 06:36:54 -0500

    cambio2

--- src/components/About.js
+++ src/components/About.js
@@ -11,12 +11,12 @@ const About = () => {
   return ( 
   <section className='section' id='about' ref={ref}>
     <div className='container mx-auto'>
-      <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center'> 
+      <div className='flex flex-col gap-y-10 lg:flex-row-reverse lg:items-center'> 
         {/*img */}
-        <motion.div  variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3 }} className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+        <motion.div  variants={fadeIn('left', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3 }} className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
         </motion.div>
         {/*text*/}
-        <motion.div variants={fadeIn('left', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3 }} className='flex-1'>
+        <motion.div variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.3 }} className='flex-1'>
           <h2 className='h2 text-accent'> About me.</h2>
           <h3 className='h3 mb-4'> Soy Desarrollador Front end </h3>
           <p className='mb-6'>
