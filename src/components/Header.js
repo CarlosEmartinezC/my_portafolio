ec1ca76d8029ccf06019c50d0b7c99f0f73bc3cc
CarlosEmartinezC <133069933+CarlosEmartinezC@users.noreply.github.com>
2023-09-12 06:35:39 -0500

    cambios 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg';
 const Header = () => {
   return <header className='py-8' id='header'>
     <div className='container mx-auto'>
       <div className='flex justify-between item-center'>
         {/* logo*/}
-        <a href='#'>
+        <a href='about'>
           <img src={logo} alt=''/>
         </a>
         {/*buton*/}
-        <button className='btn btn-sm'>Work With me</button>
+        <button className='btn btn-sm'>Descarga CV </button>
       </div>
     </div>
 
